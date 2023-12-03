33629271a7a2dcd35962d9fd2ca35a061b3bca27
Marghub <marghub12@gmail.com>
2023-12-03 22:11:35 +0530

    Authorization added on save article api

--- routes/contentListRoute.js
+++ routes/contentListRoute.js
@@ -1,16 +1,16 @@
 const express = require("express");
-
 const router = express.Router();
 
 const {getAllContent, GetContentList, GetAllContentList, GetContentById, GetArticleList, getContent, SaveArticle, saveFile, PublishArticle} = require("../controller/contentListController.js");
+const { verifyAdmin } = require("../util/verifyToken.js");
 
 router.get("/getAllContent", getAllContent);
 router.get("/GetContentList/:page", GetContentList);
 router.get("/GetAllContentList/:page", GetAllContentList);
 router.get("/GetContentById/:contentId", GetContentById);
-router.get("/SaveArticle", getAllContent);
+router.get("/SaveArticle", verifyAdmin, getAllContent);
 router.get("/GetArticleList/:page", GetArticleList);
-router.get("/PublishArticle", getAllContent);
+router.get("/PublishArticle", verifyAdmin, getAllContent);
 router.get("/getContent", getContent);
 router.post("/savefile", saveFile);
 router.post("/SaveArticle", SaveArticle);
